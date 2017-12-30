619c231313b99670d27638105ec8a53280ee17d2
asmaldone <alsmaldone@yahoo.com>
2017-12-29 23:33:57 -0500

    spring mongo docker

---
+++ src/main/java/guru/springframework/SpringBootMongodbApplication.java
@@ -0,0 +1,15 @@
+package guru.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
+@SpringBootApplication
+//@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
+public class SpringBootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongodbApplication.class, args);
+	}
+}
